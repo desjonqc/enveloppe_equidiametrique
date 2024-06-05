957a57cf9413a5c91effedd957e4e44c2271518c
Clément DESJONQUERES <34980982+desjonqc@users.noreply.github.com>
2024-06-05 23:12:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,40 +9,9 @@ from opencl import OpenCLProgram
 
 programme = OpenCLProgram("functions.cl")
 
-def arc_simple():
-    e = Ensemble(geometrie.arc(100, (100, 200), np.pi * 3 / 6, 300, 5))
-    methode = Calculateur1(e)
-
-    methode.afficher()
-
-    resultats = methode.iteration_enveloppe_cl(programme, 0.1, 5)
-
-    # for resultat in resultats:
-    #     plt.imshow(resultat, cmap="Greys", interpolation="nearest")
-    #     plt.show()
-
-    anim = ResultatAnimation(e, resultats)
-    anim.afficher()
-
-def evolution_cercle():
-    N = 60
-    ensembles = [Ensemble(geometrie.arc(100, (150, 150), 2 * np.pi * i / N, 300, 5)) for i in range(N)]
-
-    enveloppes = enveloppe.evolution(programme, Calculateur1, ensembles, 0.1)
-    evolution = EvolutionAnimation(enveloppes, ensembles_superposes=ensembles)
-    evolution.sauvegarder("evolution.gif")
-    evolution.afficher()
-
-
 if __name__ == "__main__":
-    ensemble = Ensemble(geometrie.importer_image("formes/texas.gif"))
-
-    methode = Calculateur1(ensemble)
-    methode.afficher()
-    figure = methode.enveloppe_equidiametrique_cl(programme, 0.3, reduite=False)
-
-    plt.imshow((figure - ensemble.matrice_de_travail(0.3)), cmap="Greys", interpolation="nearest")
-    plt.show()
+    # Simuler...
+    pass
 
 
 
